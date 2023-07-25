1d3532103d1db168a70dcd284c585928ca449dba
AnPiakhota <andrei.pehota@geomotiv.com>
2023-07-25 11:45:24 +0300

    Mantle ONFT721 mints subgraph impl

---
+++ src/mapping.ts
@@ -0,0 +1,12 @@
+import { ONFTMinted } from '../generated/UniChainsONFT721/UniChainsONFT721'
+import { ONFTMint } from '../generated/schema'
+
+export function handleONFTMinted(event: ONFTMinted): void {
+  let mint = new ONFTMint(event.params.id.toHex())
+  mint.minter = event.params.minter
+  mint.referrer = event.params.referrer
+  mint.mintId = event.params.mintId
+  mint.referralEarnings = event.params.referralEarnings
+  mint.protocolEarnings = event.params.protocolEarnings
+  mint.save()
+}
